0701ea74f73cec18aed62e4dde425a010d522094
Stéphane Menut <smenut@dawan.fr>
2022-06-07 14:56:40 +0200

    twig

--- src/Controller/TwigController.php
+++ src/Controller/TwigController.php
@@ -9,12 +9,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class TwigController extends AbstractController
 {
     /**
-     * @Route("/twig", name="twig_index", priority="-9")
+     * @Route("/twig", name="twig_index", priority="0")
      */
     public function index(): Response
     {
+        $html = '<p>Lorem ipsum dolor sit amet, <b>consectetur adipisicing elit</b>. Aliquid animi atque dignissimos enim, 
+            laudantium libero nisi obcaecati ratione sapiente. <i>Adipisci consequuntur eos excepturi explicabo</i> 
+            laboriosam laudantium nulla perferendis quas, sapiente.</p><script>console.log("coucou")</script>';
+
+
         return $this->render('twig/index.html.twig', [
-            'controller_name' => 'TwigController',
+            'users' => ['Maxime', 'Franck', 'Pierre-louis', 'Stéphane'],
+            'admins' => [],
+            'product' => ['name' => 'Pomme', 'price' => 12.99],
+            'shopIsOpen' => false,
+            'html' => $html,
+            'person' => ['name' => '', 'email' => '']
+        ]);
+    }
+
+    public function image(): Response
+    {
+        return $this->render('twig/image.html.twig', [
+            'image' => 'grogu.jpg'
+        ]);
+    }
+
+    /**
+     * @Route("/twig/message", name="twig_message")
+     */
+    public function message(): Response
+    {
+
+        $message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque dignissimos enim, 
+            laudantium libero nisi obcaecati ratione sapiente.";
+
+        return $this->render('twig/message.txt.twig', [
+            'name' => 'Admin',
+            'message' => $message
         ]);
     }
 }
